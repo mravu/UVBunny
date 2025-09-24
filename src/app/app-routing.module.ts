d8cf950baf1a66d98195e07533c96593607e1127
mravu <manaswitha.ravu@gmail.com>
2025-09-24 13:29:22 -0400

    final changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainPageComponent } from './components/main-page/main-page.component';
 import { BunnyDetailComponent } from './components/bunny-detail/bunny-detail.component';
 
 const routes: Routes = [
-  { path: '', component: MainPageComponent },
-  { path: 'bunny/:id', component: BunnyDetailComponent }
+    {
+        path: 'bunny/:id',
+        loadComponent: () =>
+          import('./components/bunny-detail/bunny-detail.component').then(m => m.BunnyDetailComponent)
+      }
 ];
 
 @NgModule({
